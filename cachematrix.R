5eaf6948d9c599428a891dbaa8c1a64e37faa9cc
Angela <schmange@gmail.com>
2014-05-05 10:54:11 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## These two functions will create a cached matrix, then set and get its mean
+## These two functions will create a cached matrix, then set and get its inverse
 
 ## This function will create a cached matrix
 
@@ -11,26 +11,26 @@ makeCacheMatrix <- function(x = matrix())
     s <<- NULL
     }
   get <- function() x
-  setmean <- function(solve) s <<- solve
-  getmean <- function() s
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
   list(set = set, get = get,
        setsolve = setsolve,
        getsolve = getsolve)
 }
 
 
-## This function will calculate the mean for the cached matrix
+## This function will calculate the inverse for the cached matrix
 
 cacheSolve <- function(x, ...)
   {
-  s <- x$getmean()
+  s <- x$getsolve()
   if(!is.null(s))
     {
     message("getting cached data")
     return(s)
     }
   data <- x$get()
-  s <- mean(data, ...)
-  x$setmean(s)
+  s <- solve(data, ...)
+  x$setsolve(s)
   s
 }
